6ecd48cb751882c1600275b92078c062345b73a4
nupur <nupur.banerjee@iiitb.org>
2021-05-05 11:58:15 +0530

    upload files component

---
+++ backend/src/main/java/com/projectelective/reports/service/FileStorageService.java
@@ -0,0 +1,43 @@
+package com.projectelective.reports.service;
+
+import com.projectelective.reports.dao.FileDBRepository;
+import com.projectelective.reports.entity.FileDB;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+
+@Service
+public class FileStorageService {
+
+    @Autowired
+    private FileDBRepository fileDBRepository;
+
+    public FileDB store(MultipartFile file) throws IOException {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+        FileDB FileDB = new FileDB(fileName, file.getContentType(), file.getBytes());
+
+        byte[] bytes = file.getBytes();
+        String blobString = new String(bytes);
+        System.out.println("----converting blob to text -----"+blobString);
+        return fileDBRepository.save(FileDB);
+    }
+
+    public FileDB getFile(String id) {
+        return fileDBRepository.findById(id).get();
+    }
+
+    public Stream<FileDB> getAllFiles() {
+        return fileDBRepository.findAll().stream();
+    }
+}
